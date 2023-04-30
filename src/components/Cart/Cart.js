17423cab41874c94913b4a32482a8d1c9e48accb
Jen <107281506+jenvdr@users.noreply.github.com>
2023-04-30 21:41:11 +0200

    Allow increasing and decreasing item(s) quantity inside cart. Include message if cart is empty

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,16 +6,19 @@ import { useSelector } from 'react-redux';
 const Cart = (props) => {
   const items = useSelector(state => state.cart.items);
 
+  const itemsAvailable = items.length !== 0;
+
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        {items.map(item => (
+        {itemsAvailable && items.map(item => (
           <CartItem
           key={item.id}
-          item={{ title: item.name, quantity: item.quantity, total: item.totalPrice, price: item.price }}
+          item={{id: item.id, title: item.name, quantity: item.quantity, total: item.totalPrice, price: item.price }}
         />
         ))}
+        {!itemsAvailable && <p>Your cart is empty.</p>}
       </ul>
     </Card>
   );
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -30,6 +30,7 @@ const cartSlice = createSlice({
     removeProduct(state, action) {
         const id = action.payload;
         const existingItem = state.items.find((item) => item.id === id);
+        state.totalQuantity--;
         if (existingItem.quantity === 1) {
             state.items = state.items.filter(item => item.id !== id);
         } else {
